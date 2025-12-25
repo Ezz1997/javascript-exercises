3fd4eb98fc82268945e2b934181f8cdc6efca9aa
Ezz1997 <ezmarie97@gmail.com>
2025-12-25 12:05:32 +0200

    Add solution to exercise 09_sumAll

--- 09_sumAll/sumAll.js
+++ 09_sumAll/sumAll.js
@@ -1,5 +1,22 @@
-const sumAll = function() {
+const sumAll = function(bound1, bound2) {
 
+    if(!Number.isInteger(bound1) || !Number.isInteger(bound2)){
+        return "ERROR";
+    }
+
+    if(bound1 < 0 || bound2 < 0){
+        return "ERROR";
+    }
+
+    let upperIncluded = Math.max(bound1, bound2);
+    let lowerIncluded = Math.min(bound1, bound2);
+    let sum = 0;
+
+    for(let i = lowerIncluded; i <= upperIncluded; i++){
+        sum += i;
+    }
+
+    return sum;
 };
 
 // Do not edit below this line
