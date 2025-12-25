e8629c7653c8a381a74e97953ded2fd8b4929e12
Ezz1997 <ezmarie97@gmail.com>
2025-12-25 13:45:21 +0200

    Add solution to exercise 10_leapYears

--- 10_leapYears/leapYears.js
+++ 10_leapYears/leapYears.js
@@ -1,5 +1,15 @@
-const leapYears = function() {
+const leapYears = function(years) {
 
+    let isDiviseableByFour = (years % 4 === 0);
+    let isDiviseableByHundred = (years % 100 === 0);
+    let isDiviseableByFourHundred = (years % 400 === 0);
+
+    if((!isDiviseableByHundred && isDiviseableByFour) || 
+       (isDiviseableByHundred && isDiviseableByFourHundred)){
+        return true;
+    }
+
+    return false;
 };
 
 // Do not edit below this line
