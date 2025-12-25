c1f9a5ae5d2e107f640486d9fb987b9311428c5c
Ezz1997 <ezmarie97@gmail.com>
2025-12-25 13:56:10 +0200

    Add solution to exercise 11_tempConversion

--- 11_tempConversion/tempConversion.js
+++ 11_tempConversion/tempConversion.js
@@ -1,7 +1,15 @@
-const convertToCelsius = function() {
+const convertToCelsius = function(temp) {
+  let celsius = (temp - 32) * (5 / 9);
+  let roundedOneDecimal = Math.round(celsius * 10) / 10;
+
+  return roundedOneDecimal;
 };
 
-const convertToFahrenheit = function() {
+const convertToFahrenheit = function(temp) {
+  let fahrenheit = (temp * 9 / 5) + 32;
+  let roundedOneDecimal = Math.round(fahrenheit * 10) / 10;
+
+  return roundedOneDecimal;
 };
 
 // Do not edit below this line
