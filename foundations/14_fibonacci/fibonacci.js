2e36cccf1fedcb105b79f14dcbd1b6fd34d37195
Ezz1997 <ezmarie97@gmail.com>
2026-01-19 15:02:07 +0200

    Add solution to exercise 14

--- foundations/14_fibonacci/fibonacci.js
+++ foundations/14_fibonacci/fibonacci.js
@@ -1,5 +1,19 @@
-const fibonacci = function() {
+const fibonacci = function(num) {
 
+    if(num < 0){
+        return "OOPS";
+    }
+
+    let cur = 0;
+    let next = 1;
+
+    for(let i = 0; i < num; i++){
+        const sum = cur + next;
+        cur = next;
+        next = sum;
+    }
+
+    return cur;
 };
 
 // Do not edit below this line
