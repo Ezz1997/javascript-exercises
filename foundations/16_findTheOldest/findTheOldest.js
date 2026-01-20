f0f540ddc196e7cfef019ca557c3fb47f330fefa
Ezz1997 <ezmarie97@gmail.com>
2026-01-20 15:17:27 +0200

    Add solution to exercise 16

--- foundations/16_findTheOldest/findTheOldest.js
+++ foundations/16_findTheOldest/findTheOldest.js
@@ -1,5 +1,22 @@
-const findTheOldest = function() {
+const getAge = function (birth, death) {
+  if (!death) {
+    death = new Date().getFullYear();
+  }
+  return death - birth;
+};
 
+const findTheOldest = function(people) {    
+    return people.reduce((oldestPerson, person) => {
+        const currentAge = getAge(person.yearOfBirth, person.yearOfDeath);
+        const oldestPersonAge = getAge(oldestPerson.yearOfBirth, oldestPerson.yearOfDeath);
+
+            if(oldestPersonAge < currentAge){
+                return person;
+            }
+        
+
+        return oldestPerson;
+    });
 };
 
 // Do not edit below this line
